8a427ba286e1c06f31f5492de80a78123b811029
Hector Biminchumo <hector.biminchumo@externos-cl.cencosud.com>
2024-01-24 22:26:06 -0500

    adding user input validation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,14 +1,36 @@
 const User = require('../models/user.model');
 const authUtil = require('../util/authentication');
+const validation = require('../util/validation');
 
 function getSignup(req, res) {
     res.render('customer/auth/signup');
 }
 
 async function signup(req, res, next) {
-  const user = new User(req.body.email, req.body.password, req.body.fullname, req.body.street, req.body.postal, req.body.city);
+  
+  if (!validation.userDetailsAreValid(req.body.email, req.body.password, req.body.fullname, req.body.street, req.body.postal, req.body.city) || !validation.emailIsConfirmed(req.body.email, req.body['confirm-email'])) {
+    res.redirect('/signup');
+    return;
+  }
 
-  try {
+  const user = new User(
+    req.body.email,
+    req.body.password,
+    req.body.fullname,
+    req.body.street,
+    req.body.postal,
+    req.body.city
+    );
+
+    
+    try {
+      const existsAlready = await user.existsAlready();
+    
+      if (existsAlready) {
+        res.redirect('/signup');
+        return;
+      }
+      
       await user.signup();
   } catch (error) {
     next(error);
