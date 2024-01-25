664d3177de888868d8d24371b74767e8bd674a21
Hector Biminchumo <hector.biminchumo@externos-cl.cencosud.com>
2024-01-25 17:24:50 -0500

    flashing errors & Input Data onto Sessions

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,15 +1,36 @@
 const User = require('../models/user.model');
 const authUtil = require('../util/authentication');
 const validation = require('../util/validation');
+const sessionFlash = require('../util/session-flash');
 
 function getSignup(req, res) {
     res.render('customer/auth/signup');
 }
 
 async function signup(req, res, next) {
+    const enteredData = {
+        email: req.body.email,
+        password: req.body.password,
+        fullname: req.body.fullname,
+        street: req.body.street,
+        postal: req.body.postal,
+        city: req.body.city,    
+    }
   
-  if (!validation.userDetailsAreValid(req.body.email, req.body.password, req.body.fullname, req.body.street, req.body.postal, req.body.city) || !validation.emailIsConfirmed(req.body.email, req.body['confirm-email'])) {
-    res.redirect('/signup');
+  if (!validation.userDetailsAreValid(
+    req.body.email,
+    req.body.password,
+    req.body.fullname,
+    req.body.street,
+    req.body.postal,
+    req.body.city) || 
+    !validation.emailIsConfirmed(req.body.email, req.body['confirm-email'])) {
+    sessionFlash.flashDataToSession(req, {
+        errorMessage: 'Please check your input. Password must be at least 6 characters long, postal code must be 5 characters long',
+        ...enteredData
+    }, function() {
+        res.redirect('/signup');
+    })    
     return;
   }
 
@@ -27,7 +48,14 @@ async function signup(req, res, next) {
       const existsAlready = await user.existsAlready();
     
       if (existsAlready) {
-        res.redirect('/signup');
+        sessionFlash.flashDataToSession(req, {
+            errorMessage: 'User exists already! Try logging in instead!',
+            ...enteredData,
+        },
+        function () {
+            res.redirect('/signup');
+        }
+        );
         return;
       }
       
@@ -54,8 +82,16 @@ async function login(req, res){
         return;
     }
 
+    const sessionErrorData = {
+        errorMessage: 'Invalid credentials = please double-check your email and password!',
+        email: user.email,
+        password: user.password
+    }
+
     if (!existingUser) {
-        res.redirect('/login');
+        sessionFlash.flashDataToSession(req, sessionErrorData, function() {
+            res.redirect('/login');
+        })
         return;
     }
 
