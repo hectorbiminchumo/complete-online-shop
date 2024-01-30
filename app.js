164b9eea14a5f0e987cb4c3d77621ec615a7e518
Hector Biminchumo <hector.biminchumo@externos-cl.cencosud.com>
2024-01-30 15:57:24 -0500

    fetching & outputting product items

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static('public'))
-
+app.use(express.static('public'));
+app.use('/products/assets',express.static('product-data'));
 app.use(express.urlencoded({ extended: false}))
 
 const sessionConfig = createSessionConfig()
