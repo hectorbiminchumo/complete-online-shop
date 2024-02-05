32837a932348f08167796cf808a16594e521abcd
Hector Biminchumo <hector.biminchumo@externos-cl.cencosud.com>
2024-02-05 09:44:12 -0500

    Various fixes and proper route protection

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -6,6 +6,12 @@ router.get('/', function(req, res) {
   res.redirect('/products')
 });
 
+router.get('/401', function(req, res) {
+  res.render('shared/401')
+})
+router.get('/403', function(req, res) {
+  res.render('shared/403')
+})
 
 
 module.exports = router;
