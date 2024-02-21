7a0666426c873ae5d6207765a24979be5a0adfa9
Hector Biminchumo <hector.biminchumo@externos-cl.cencosud.com>
2024-02-21 14:42:41 -0500

    Configuring the Stripe request & handling payments

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,3 +1,4 @@
+const stripe = require('stripe')('sk_test_51K9CgdEoBsB9WBkV6FMXcqBUSBzkI7x3WU3JQww7ZbHV6kjUeGqnemZd9N82uneplcWRekG37D30gYi9eAMl03rc00LXHSHal4');
 const Order = require('../models/order.model');
 const User = require('../models/user.model');
 
@@ -32,10 +33,39 @@ async function addOrder(req, res, next) {
 
   req.session.cart = null;
 
-  res.redirect('/orders');
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: cart.items.map(function(item) {
+      return {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: item.product.title
+          },
+          unit_amount_decimal: +item.product.price.toFixed(2) * 100
+        },
+        quantity: item.quantity,
+      }
+  }),
+    mode: 'payment',
+    success_url: `http://localhost:3000/orders/success`,
+    cancel_url: `http://localhost:3000/orders/failure`,
+  });
+
+  res.redirect(303, session.url);
+}
+
+function getSuccess(req, res) {
+  res.render('customer/orders/success')
+}
+function getFailure(req, res) {
+  res.render('customer/orders/failure')
 }
 
 module.exports = {
   addOrder: addOrder,
   getOrders: getOrders,
+  getSuccess: getSuccess,
+  getFailure: getFailure
 };
